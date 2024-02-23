1890a7128bf8d600551a04472c2b44d69fced9bc
Valéry Febvre <vfebvre@easter-eggs.com>
2024-02-23 15:39:35 +0100

    [Servers] Asura Scans (EN): Update
    A random number is now added at begin of slug and it changes regularly.
    
    This fix partially corrects the problem by allowing the slug to be 
    updated when an update is done.
    
    Fixed issue #688

--- komikku/servers/asurascans/__init__.py
+++ komikku/servers/asurascans/__init__.py
@@ -20,6 +20,27 @@ class Asurascans(MangaStream):
 
     ignored_pages = ['page100-10.jpg', 'zzzzzzz999999.jpg', ]
 
+    def check_slug(self, initial_data):
+        # A random number is always prepended to slug and it changes regularly
+        # Try to retrieve new slug
+        res = self.search(initial_data['name'], '')
+        if not res:
+            return None
+
+        for item in res:
+            base_slug = '-'.join(initial_data['slug'].split('-')[1:])
+            current_base_slug = '-'.join(item['slug'].split('-')[1:])
+            if current_base_slug in (initial_data['slug'], base_slug) and initial_data['slug'] != item['slug']:
+                return item['slug']
+
+        return None
+
+    def get_manga_data(self, initial_data):
+        if new_slug := self.check_slug(initial_data):
+            initial_data['slug'] = new_slug
+
+        return MangaStream.get_manga_data(self, initial_data)
+
 
 class Asurascans_tr(MangaStream):
     id = 'asurascans_tr'
--- komikku/servers/perfscan/__init__.py
+++ komikku/servers/perfscan/__init__.py
@@ -18,8 +18,8 @@ class Perfscan(Heancms):
     synopsis_css_path = 'div div.container.px-5.text-gray-50 div.grid.grid-cols-12.pt-3.gap-x-3 div.col-span-12.flex.flex-col.gap-y-3 div.bg-gray-800.text-gray-50.rounded-xl.p-5'
 
     def check_slug(self, initial_data):
-        # A random number is always added to slug and it changes regulary
-        # Try to retrieve new slug.
+        # A random number is always added to slug and it changes regularly
+        # Try to retrieve new slug
         res = self.search(initial_data['name'])
         if not res:
             return None
@@ -27,8 +27,10 @@ class Perfscan(Heancms):
         for item in res:
             base_slug = '-'.join(initial_data['slug'].split('-')[:-1])
             current_base_slug = '-'.join(item['slug'].split('-')[:-1])
-            if current_base_slug == base_slug and initial_data['slug'] != item['slug']:
+            if current_base_slug in (initial_data['slug'], base_slug) and initial_data['slug'] != item['slug']:
                 return item['slug']
+
+        return None
 
     def get_manga_data(self, initial_data):
         if new_slug := self.check_slug(initial_data):
