80a6cbd42f5636e06fb6d46d5160605319cee445
Valéry Febvre <vfebvre@easter-eggs.com>
2024-02-25 22:43:16 +0100

    [Servers] Refine 4de7b79d
    - Improve manga URL
    - Fix chapters order
    - Add latest updates
    - Code cleanup
    - Add functional test

--- komikku/servers/rawdevart/__init__.py
+++ komikku/servers/rawdevart/__init__.py
@@ -3,8 +3,9 @@
 # Author: Zhao Se <zhaose233@outlook.com>
 
 import json
-import requests
+
 from bs4 import BeautifulSoup
+import requests
 
 from komikku.servers import Server
 from komikku.servers import USER_AGENT
@@ -16,13 +17,15 @@ class Rawdevart(Server):
     id = 'rawdevart'
     name = 'Rawdevart'
     lang = 'ja'
+
     is_nsfw = True
 
     base_url = 'https://rawdevart.art'
-    search_url = 'https://rawdevart.art/ajax/search-manga'
-    search_url_webpage = 'https://rawdevart.art/search?query={0}'
-    manga_url = 'https://rawdevart.art/spa/manga/{0}'
-    chapter_url = manga_url + '/{1}'
+    manga_url = base_url + '/{0}'
+    api_search_url = base_url + '/ajax/search-manga'
+    api_latest_updates_url = base_url + '/spa/latest-manga'
+    api_manga_url = base_url + '/spa/manga/{0}'
+    api_chapter_url = api_manga_url + '/{1}'
 
     def __init__(self):
         if self.session is None:
@@ -33,61 +36,66 @@ class Rawdevart(Server):
         """
         Get full manga data by parsing the response of the API.
         """
-        r = self.session_get(self.manga_url.format(initial_data['slug']))
+        r = self.session_get(self.api_manga_url.format(initial_data['slug']))
         if r.status_code != 200:
             return None
-        r_decoded = json.loads(r.content)
+
+        r_decoded = json.loads(r.text)
 
         authors = [
             author_info['author_name'] for author_info in r_decoded['authors']
         ]
-        synopsis = r_decoded['detail']['manga_description']
         genres = [tag_info['tag_name'] for tag_info in r_decoded['tags']]
+        synopsis = r_decoded['detail']['manga_description']
 
         chapters = []
-        for chapter_info in r_decoded['chapters']:
-            slug = chapter_info['chapter_number']
-            title = "Chapter " + str(slug)
-            date = convert_date_string(chapter_info['chapter_date_published'])
+        for chapter_info in reversed(r_decoded['chapters']):
+            slug = str(chapter_info['chapter_number'])
+            title = 'Chapter ' + slug
+            date = convert_date_string(chapter_info['chapter_date_published'].split('T')[0])
 
-            chapters.append(dict(slug=str(slug), title=title, date=date))
+            chapters.append(dict(slug=slug, title=title, date=date))
 
-        data = dict(name=r_decoded['detail']['manga_name'],
-                    slug=initial_data['slug'],
-                    url=self.search_url_webpage.format(r_decoded['detail']['manga_name']),
-                    authors=authors,
-                    genres=genres,
-                    synopsis=synopsis,
-                    chapters=chapters,
-                    server_id=self.id,
-                    cover=r_decoded['detail']['manga_cover_img_full'])
+        # Slug can be used to compute manga URL
+        # Manga URL must be computed and recorded
+        # It's computed using part of cover URL
+        manga_url_slug_split = r_decoded['detail']['manga_cover_img_full'].split('/')[-1].split('-')[:-1]
+        manga_url_slug_split[-1] = 'c' + manga_url_slug_split[-1]
+        manga_url_slug = '-'.join(manga_url_slug_split)
 
-        return data
+        return dict(
+            name=r_decoded['detail']['manga_name'].strip(),
+            slug=initial_data['slug'],
+            url=self.manga_url.format(manga_url_slug),
+            authors=authors,
+            genres=genres,
+            status=None,  # not available
+            synopsis=synopsis,
+            chapters=chapters,
+            server_id=self.id,
+            cover=r_decoded['detail']['manga_cover_img_full'],
+        )
 
-    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug,
-                               chapter_url):
+    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
         """
         Returns manga chapter data by parsing the response of the API.
         """
-        r = self.session_get(self.chapter_url.format(manga_slug, chapter_slug))
+        r = self.session_get(self.api_chapter_url.format(manga_slug, chapter_slug))
         if r.status_code != 200:
             return None
-        r_decoded = json.loads(r.content)
 
-        soup = BeautifulSoup(r_decoded['chapter_detail']['chapter_content'],
-                             "html.parser")
+        r_decoded = json.loads(r.text)
+
+        soup = BeautifulSoup(r_decoded['chapter_detail']['chapter_content'], 'lxml')
+
         pages = []
-        index = 1
-        for canvas in soup.find_all('canvas'):
+        for index, canvas in enumerate(soup.find_all('canvas')):
             image = canvas.get('data-srcset')
-            pages.append(dict(image=image, index=index))
-            index += 1
+            pages.append(dict(image=image, index=index + 1))
 
-        data = dict(pages=pages)
-        return data
+        return dict(pages=pages)
 
-    def get_manga_chapter_page_image(self, manga_slug, manga_name,
-                                     chapter_slug, page):
+    def get_manga_chapter_page_image(self, manga_slug, manga_name, chapter_slug, page):
         """
         Returns chapter page scan (image) content
         """
@@ -102,33 +110,60 @@ class Rawdevart(Server):
         return dict(
             buffer=r.content,
             mime_type=mime_type,
-            name='{0:03d}.{1}'.format(page['index'],
-                                      mime_type.split('/')[-1]),
+            name='{0:03d}.{1}'.format(page['index'], mime_type.split('/')[-1]),
         )
 
     def get_manga_url(self, slug, url):
         return url  # It's hard to get the absolute webpage url
 
+    def get_latest_updates(self):
+        """
+        Returns latest updates
+        """
+        r = self.session_get(self.api_latest_updates_url)
+        if r.status_code != 200:
+            return None
+
+        r_decoded = json.loads(r.text)
+
+        results = []
+        for manga_info in r_decoded['manga_list']:
+            slug = manga_info['manga_id']
+            name = manga_info['manga_name']
+            cover = manga_info['manga_cover_img']
+            last_chapter = 'Chapter ' + str(manga_info['manga_chapters'][0]['chapter_number'])
+
+            results.append(dict(
+                slug=str(slug),
+                name=name,
+                cover=cover,
+                last_chapter=last_chapter,
+            ))
+
+        return results
+
     def search(self, term=None):
         """
         Search for mangas
         """
-        r = self.session_get(self.search_url, params=dict(query=term))
+        r = self.session_get(self.api_search_url, params=dict(query=term))
         if r.status_code != 200:
             return None
-        r_decoded = json.loads(r.content)
 
-        data = []
+        r_decoded = json.loads(r.text)
+
+        results = []
         for manga_info in r_decoded:
             slug = manga_info['manga_id']
             name = manga_info['manga_name']
             cover = manga_info['manga_cover_img']
             nb_chapters = manga_info['chapter_number']
 
-            data.append(
-                dict(slug=str(slug),
-                     name=name,
-                     cover=cover,
-                     nb_chapters=nb_chapters))
+            results.append(dict(
+                slug=str(slug),
+                name=name,
+                cover=cover,
+                nb_chapters=nb_chapters,
+            ))
 
-        return data
+        return results
