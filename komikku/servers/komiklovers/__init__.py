f679702df40fd7eb1f589fc09ba75e3189db0fed
Valéry Febvre <vfebvre@easter-eggs.com>
2024-04-12 16:15:48 +0200

    [Servers] Add KomikLovers (ID)

---
+++ komikku/servers/komiklovers/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019-2024 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+from komikku.servers.multi.manga_stream import MangaStream
+
+
+class Komiklovers(MangaStream):
+    id = 'komiklovers'
+    name = 'KomikLovers'
+    lang = 'id'
+
+    series_name = 'komik'
+
+    base_url = 'https://komiklovers.com'
+
+    authors_selector = '.infotable tr:-soup-contains("Artist") td:last-child, .infotable tr:-soup-contains("Author") td:last-child'
+    genres_selector = '.seriestugenre a'
+    scanlators_selector = None
+    status_selector = '.seriestucontent table tr:-soup-contains("Status") td:last-child'
+    synopsis_selector = '[itemprop="description"]'
