4cf5a46c0a0803cbe5c5c797166c25e7c3f0aed3
Valéry Febvre <vfebvre@easter-eggs.com>
2024-04-17 16:42:19 +0200

    [Servers] Add Cartel De Manhwas (ES)

---
+++ komikku/servers/carteldemanhwas/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2019-2024 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+from komikku.servers.multi.manga_stream import MangaStream
+
+
+class Carteldemanhwas(MangaStream):
+    id = 'carteldemanhwas'
+    name = 'Cartel De Manhwas'
+    lang = 'es'
+    is_nsfw = True
+
+    series_name = 'series'
+
+    base_url = 'https://carteldemanhwas.com'
+
+    authors_selector = '.infotable tr:-soup-contains("Artist") td:last-child, .infotable tr:-soup-contains("Author") td:last-child'
+    genres_selector = '.seriestugenre a'
+    scanlators_selector = None
+    status_selector = '.seriestucontent table tr:-soup-contains("Status") td:last-child'
+    synopsis_selector = '[itemprop="description"]'
