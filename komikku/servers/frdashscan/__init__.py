c99c7f801d5fce424ebca290b787ec142e3d7b17
Valéry Febvre <vfebvre@easter-eggs.com>
2024-05-27 23:31:21 +0200

    [Servers] Fr-Scan (FR): Update
    Use custom get_manga_chapter_data() method

--- komikku/servers/frdashscan/__init__.py
+++ komikku/servers/frdashscan/__init__.py
@@ -2,7 +2,13 @@
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
+import json
+import re
+
+from bs4 import BeautifulSoup
+
 from komikku.servers.multi.madara import Madara
+from komikku.servers.utils import get_buffer_mime_type
 
 
 class Frdashscan(Madara):
@@ -16,3 +22,44 @@ class Frdashscan(Madara):
     base_url = 'https://fr-scan.com'
     chapter_url = base_url + '/manga/{0}/{1}/'  # don't support style param
     chapters_url = base_url + '/manga/{0}/ajax/chapters/'
+
+    images_regex = r'var chapter_preloaded_images *= *(\[.*\]).*'
+
+    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
+        r = self.session_get(
+            self.chapter_url.format(manga_slug, chapter_slug),
+            headers={
+                'Referer': self.manga_url.format(manga_slug),
+            }
+        )
+        if r.status_code != 200:
+            return None
+
+        mime_type = get_buffer_mime_type(r.content)
+        if mime_type != 'text/html':
+            return None
+
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        if script_element := soup.select_one('script#chapter_preloaded_images'):
+            data = dict(
+                pages=[],
+            )
+
+            script = script_element.string.strip()
+            for line in script.split(';'):
+                line = line.strip()
+                if not line.startswith('var chapter_preloaded_images'):
+                    continue
+
+                line = line.replace('\\', '')
+                if match := re.compile(self.images_regex).search(line):
+                    for url in json.loads(match.group(1)):
+                        data['pages'].append(dict(
+                            slug=None,
+                            image=url,
+                        ))
+
+                return data
+        else:
+            return Madara.get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url)
