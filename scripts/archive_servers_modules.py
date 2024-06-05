4e0e39ae1e59b85e13d3deb4bb0e8bb4525bee5e
Valéry Febvre <vfebvre@easter-eggs.com>
2024-06-06 01:08:59 +0200

    [Scripts] Refine generate_servers_index.py

--- scripts/archive_servers_modules.py
+++ scripts/archive_servers_modules.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 # Create a ZIP archive of all modules: {dirpath}/servers.zip
 
--- scripts/generate_servers_index.py
+++ scripts/generate_servers_index.py
@@ -1,20 +1,25 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import glob
 import hashlib
 import json
 import os
-import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+dirpath = 'komikku/servers'
 
-from komikku.servers import APP_MIN_VERSION
+# Extract minimum app version to use modules
+with open(os.path.join(dirpath, '__init__.py')) as fp:
+    for line in fp.readlines():
+        if line.startswith('APP_MIN_VERSION'):
+            app_min_version = line.split()[2].strip().replace("'", '')
+            if app_min_version == 'None':
+                app_min_version = None
+            break
 
 data = {
-    'app_min_version': APP_MIN_VERSION,
+    'app_min_version': app_min_version,
     'modules': {},
 }
-dirpath = 'komikku/servers'
 
 # Walk in servers folder
 for pathname in ('komikku/servers/multi/*/', 'komikku/servers/*/'):
