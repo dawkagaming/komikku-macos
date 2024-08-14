f6b0468a76c469e6bc1bc81840e6c8310acd2b4f
Valéry Febvre <vfebvre@easter-eggs.com>
2024-08-14 14:12:27 +0200

    [Servers] Add Starbound Scans (FR)

---
+++ komikku/servers/starboundscans/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2019-2024 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+from komikku.servers.multi.keyoapp import Keyoapp
+
+
+class Starboundscans(Keyoapp):
+    id = 'starboundscans'
+    name = 'Starbound Scans'
+    lang = 'fr'
+
+    base_url = 'https://starboundscans.com'
