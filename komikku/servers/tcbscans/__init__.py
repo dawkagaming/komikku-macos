0b52d5255d0f1c49d06d95d240a38078945fc967
Valéry Febvre <vfebvre@easter-eggs.com>
2025-02-27 22:33:59 +0100

    [Servers] TCB Scans (EN): Update
    Domain change

--- komikku/servers/tcbscans/__init__.py
+++ komikku/servers/tcbscans/__init__.py
@@ -19,7 +19,7 @@ class Tcbscans(Server):
     name = 'TCB Scans'
     lang = 'en'
 
-    base_url = 'https://tcbscans.me'
+    base_url = 'https://tcbonepiecechapters.com'
     most_populars_url = base_url + '/projects'
     manga_url = base_url + '/mangas/{0}'
     chapter_url = base_url + '/chapters/{0}'
