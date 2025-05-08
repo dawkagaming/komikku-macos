dawkagaming/komikku-macos

commit b5b9a319bf38f141b7768203e911e0c380658fed

[Reader] Code cleanup

komikku/reader/pager/infinite_canvas.py
@@ -39,6 +39,8 @@ class KInfiniteCanvas(Gtk.Widget, Gtk.Scrollable):
         self.__hadj = None
         self.__vadj = None
 
+        self.vadjustment_value_changed_handler_id = None
+
         self.scroll_adjusting_delta = 0
         self.scroll_direction = None
         self.scroll_drag_offset = 0
@@ -89,12 +91,15 @@ class KInfiniteCanvas(Gtk.Widget, Gtk.Scrollable):
 
     @GObject.Property(type=Gtk.Adjustment)
     def vadjustment(self):
-        return self.__vadj
+        return self.__vadj or Gtk.Adjustment()
 
     @vadjustment.setter
     def vadjustment(self, adj):
-        self.vadjustment_value_changed_handler_id = adj.connect('value-changed', lambda adj: self.queue_allocate())
+        if not adj:
+            return
+
         self.__vadj = adj
+        self.vadjustment_value_changed_handler_id = adj.connect('value-changed', lambda _adj: self.queue_allocate())
 
     @GObject.Property(type=Gtk.ScrollablePolicy, default=Gtk.ScrollablePolicy.MINIMUM)
     def vscroll_policy(self):
@@ -186,9 +191,6 @@ class KInfiniteCanvas(Gtk.Widget, Gtk.Scrollable):
         self.queue_allocate()
 
     def configure_adjustments(self):
-        if self.vadjustment is None:
-            return
-
         lower = 0
         if first_page := self.get_first_child():
             if first_page.status == 'offlimit' and self.scroll_direction in (None, Gtk.DirectionType.UP):
@@ -216,9 +218,6 @@ class KInfiniteCanvas(Gtk.Widget, Gtk.Scrollable):
         self.scroll_adjusting_delta = 0
 
     def connect_signals(self):
-        if self.vadjustment:
-            self.vadjustment_value_changed_handler_id = self.vadjustment.connect('value-changed', lambda adj: self.queue_allocate())
-
         # Keyboard navigation
         self.key_pressed_handler_id = self.pager.window.controller_key.connect('key-pressed', self.on_key_pressed)
 

komikku/reader/pager/image.py
@@ -89,6 +89,9 @@ class KImage(Gtk.Widget, Gtk.Scrollable):
         self.animation_iter = None
         self.animation_tick_callback_id = None
 
+        self.hadjustment_value_changed_handler_id = None
+        self.vadjustment_value_changed_handler_id = None
+
         self.gesture_click_timeout_id = None
         self.obscured = True
         self.pointer_position = None  # current pointer position
@@ -161,8 +164,8 @@ class KImage(Gtk.Widget, Gtk.Scrollable):
         if not adj:
             return
 
-        adj.connect('value-changed', lambda adj: self.queue_draw())
         self.__hadj = adj
+        self.hadjustment_value_changed_handler_id = adj.connect('value-changed', lambda _adj: self.queue_draw())
         self.configure_adjustments()
 
     @GObject.Property(type=Gtk.ScrollablePolicy, default=Gtk.ScrollablePolicy.MINIMUM)
@@ -300,8 +303,9 @@ class KImage(Gtk.Widget, Gtk.Scrollable):
     def vadjustment(self, adj):
         if not adj:
             return
-        adj.connect('value-changed', lambda adj: self.queue_draw())
+
         self.__vadj = adj
+        self.vadjustment_value_changed_handler_id = adj.connect('value-changed', lambda _adj: self.queue_draw())
         self.configure_adjustments()
 
     @GObject.Property(type=Gtk.ScrollablePolicy, default=Gtk.ScrollablePolicy.MINIMUM)
@@ -503,6 +507,11 @@ class KImage(Gtk.Widget, Gtk.Scrollable):
         return self.textures
 
     def dispose(self):
+        if self.hadjustment_value_changed_handler_id:
+            self.hadjustment.disconnect(self.hadjustment_value_changed_handler_id)
+        if self.vadjustment_value_changed_handler_id:
+            self.vadjustment.disconnect(self.vadjustment_value_changed_handler_id)
+
         if self.zoomable:
             self.controller_motion.disconnect_by_func(self.on_pointer_motion)
             self.controller_scroll.disconnect_by_func(self.on_scroll)
@@ -514,6 +523,7 @@ class KImage(Gtk.Widget, Gtk.Scrollable):
         if self.animation_tick_callback_id:
             self.remove_tick_callback(self.animation_tick_callback_id)
 
+        self.data = None
         self.pixbuf = None
         self.animation_iter = None
         self.textures = None

commit 707bc201b761599d1d8b7f4399ceb6b19d16bd4e

[Reader] Fix regression
Introduced in b5b9a319bf

Close #805

komikku/reader/pager/infinite_canvas.py
@@ -218,12 +218,18 @@ class KInfiniteCanvas(Gtk.Widget, Gtk.Scrollable):
         self.scroll_adjusting_delta = 0
 
     def connect_signals(self):
+        if self.vadjustment and self.vadjustment_value_changed_handler_id is None:
+            self.vadjustment_value_changed_handler_id = self.vadjustment.connect('value-changed', lambda adj: self.queue_allocate())
+
         # Keyboard navigation
         self.key_pressed_handler_id = self.pager.window.controller_key.connect('key-pressed', self.on_key_pressed)
 
     def disconnect_signals(self):
         self.vadjustment.disconnect(self.vadjustment_value_changed_handler_id)
+        self.vadjustment_value_changed_handler_id = None
+
         self.pager.window.controller_key.disconnect(self.key_pressed_handler_id)
+        self.key_pressed_handler_id = None
 
     def dispose(self):
         self.disconnect_signals()