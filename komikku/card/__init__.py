dawkagaming/komikku-macos

commit 0cc6913494b45e9d7d9fb22a1991ab1fd6955f6b

[Card] Add new backdrop method
Blurred cover

komikku/card/__init__.py
@@ -46,6 +46,7 @@ class CardPage(Adw.NavigationPage):
     chapters_selection_mode_clear_reset_button = Gtk.Template.Child('chapters_selection_mode_clear_reset_button')
     chapters_selection_mode_menubutton = Gtk.Template.Child('chapters_selection_mode_menubutton')
     info_scrolledwindow = Gtk.Template.Child('info_scrolledwindow')
+    backdrop_picture = Gtk.Template.Child('backdrop_picture')
     title_box = Gtk.Template.Child('title_box')
     cover_box = Gtk.Template.Child('cover_box')
     name_label = Gtk.Template.Child('name_label')
@@ -417,6 +418,8 @@ class CardPage(Adw.NavigationPage):
 
     def remove_backdrop(self):
         self.remove_css_class('backdrop')
+        self.backdrop_picture.set_paintable(None)
+        self.backdrop_picture.set_opacity(1)
 
     def set_actions_enabled(self, enabled):
         self.delete_action.set_enabled(enabled)
@@ -424,12 +427,28 @@ class CardPage(Adw.NavigationPage):
         self.sort_order_action.set_enabled(enabled)
 
     def set_backdrop(self):
-        if not self.manga or Adw.StyleManager.get_default().get_high_contrast() or not Settings.get_default().card_backdrop:
+        self.remove_backdrop()
+
+        method = Settings.get_default().card_backdrop_method
+        if not self.manga or Adw.StyleManager.get_default().get_high_contrast() or not method:
             return
 
-        if backdrop_colors_css := self.manga.backdrop_colors_css:
-            self.css_provider.load_from_string(backdrop_colors_css)
-            self.add_css_class('backdrop')
+        if method == 'blurred-cover':
+            if path := self.manga.backdrop_image_fs_path:
+                self.backdrop_picture.set_filename(path)
+
+                if info := self.manga.backdrop_info:
+                    if Adw.StyleManager.get_default().get_dark():
+                        opacity = 1 - info['luminance'][0]
+                    else:
+                        opacity = info['luminance'][1]
+                    self.backdrop_picture.set_opacity(opacity)
+
+        elif method == 'linear-gradient':
+            if css := self.manga.backdrop_colors_css:
+                self.css_provider.load_from_string(css)
+
+        self.add_css_class('backdrop')
 
     def set_unread_chapters_badge(self):
         # Show unread chapters (with Adw.ViewStackPage badge) if any

data/css/style.css
@@ -109,6 +109,9 @@ viewswitcher indicatorbin > indicator {
     transition-property: background;
 }
 .backdrop button:focus:focus-visible,
+.backdrop row {
+	border-bottom: 1px solid rgb(from currentColor r g b / calc(alpha * 0.1));
+}
 .backdrop row:focus:focus-visible {
     outline-color: rgb(from currentColor r g b / calc(alpha * 0.25));
 }

data/info.febvre.Komikku.gschema.xml.in
@@ -6,6 +6,12 @@
         <value nick="gray" value="2" />
         <value nick="system-style" value="3" />
     </enum>
+    <enum id="@appid@.CardBackdropMethod">
+        <value nick="unset" value="-1" />
+        <value nick="none" value="0" />
+        <value nick="linear-gradient" value="1" />
+        <value nick="blurred-cover" value="2" />
+    </enum>
     <enum id="@appid@.ColorScheme">
         <value nick="light" value="0" />
         <value nick="dark" value="1" />
@@ -99,7 +105,12 @@
         <key type="b" name="card-backdrop">
             <default>false</default>
             <summary>Backdrop in Details Page</summary>
-            <description>Enable background recoloring to match cover</description>
+            <description>Enable background recoloring to match cover (DEPRECATED since 1.79.0)</description>
+        </key>
+        <key enum="@appid@.CardBackdropMethod" name="card-backdrop-method">
+            <default>"unset"</default>
+            <summary>Backdrop in Details Page</summary>
+            <description>Method used to recolor background to match cover</description>
         </key>
 
         <key type="b" name="desktop-notifications">

data/ui/preferences.blp
@@ -37,9 +37,16 @@ template $PreferencesDialog : Adw.PreferencesDialog {
         subtitle: _("Use system accent colors");
       }
 
-      Adw.SwitchRow card_backdrop_switch {
+      Adw.ComboRow card_backdrop_method_row {
         title: _("Backdrop in Details Page");
-        subtitle: _("Enable background recoloring to match cover");
+        subtitle: _("Method used to recolor background to match cover");
+        model: StringList {
+          strings [
+            _("None"),
+            _("Linear gradient"),
+            _("Blurred cover"),
+          ]
+        };
       }
     }
 

komikku/models/database/mangas.py
@@ -6,12 +6,16 @@ import datetime
 from gettext import gettext as _
 import gc
 import importlib
+import json
 import logging
 import os
 import shutil
 import time
 
 from modern_colorthief import get_palette
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageStat
 
 from komikku.models.database import create_db_connection
 from komikku.models.database import insert_row
@@ -145,6 +149,45 @@ class Manga:
             fp.writelines(colors)
 
         return ''.join(colors)
+
+    @property
+    def backdrop_image_fs_path(self):
+        if self.cover_fs_path is None:
+            return None
+
+        path = os.path.join(self.path, 'backdrop_image.jpg')
+        if os.path.exists(path):
+            return path
+
+        with Image.open(self.cover_fs_path) as image:
+            image = image.convert('RGB').filter(ImageFilter.GaussianBlur(35))
+            image.save(path, 'JPEG')
+
+        return path
+
+    @property
+    def backdrop_info(self):
+        if self.backdrop_image_fs_path is None:
+            return None
+
+        path = os.path.join(self.path, 'backdrop_info.json')
+        if os.path.exists(path):
+            with open(path) as fp:
+                return json.load(fp)
+
+        with Image.open(self.backdrop_image_fs_path) as image:
+            stat = ImageStat.Stat(image.convert('L'))
+            info = {
+                # Luninance values used to apply an opacity on Picture depending of color scheme (dark/light)
+                'luminance': [
+                    min((stat.mean[0] + stat.extrema[0][0]) / 510, 0.7),
+                    max((stat.mean[0] + stat.extrema[0][1]) / 510, 0.3),
+                ]
+            }
+            with open(path, 'w') as fp:
+                json.dump(info, fp)
+
+        return info
 
     @property
     def categories(self):

komikku/models/settings.py
@@ -18,7 +18,7 @@ class Settings(Gio.Settings):
     instance = None
 
     def __init__(self):
-        Gio.Settings.__init__(self)
+        super().__init__()
 
     @staticmethod
     def new():
@@ -80,12 +80,44 @@ class Settings(Gio.Settings):
         self.set_boolean('borders-crop', state)
 
     @property
-    def card_backdrop(self):
-        return self.get_boolean('card-backdrop')
+    def card_backdrop_method(self):
+        """Return the card's backdrop method """
+        value = self.card_backdrop_method_value
 
-    @card_backdrop.setter
-    def card_backdrop(self, state):
-        self.set_boolean('card-backdrop', state)
+        if value == 0:
+            return None
+        if value == 1:
+            return 'linear-gradient'
+        if value == 2:
+            return 'blurred-cover'
+
+    @property
+    def card_backdrop_method_value(self):
+        """Return the card's backdrop method value"""
+        value = self.get_enum('card-backdrop-method')
+
+        if value == -1:
+            # Before 1.79.0, value was a boolean and key was `card-backdrop`
+            # Convert old value if value is unset (-1)
+            old_value = self.get_boolean('card-backdrop')
+            value = 1 if old_value else 0
+
+        return value
+
+    @card_backdrop_method.setter
+    def card_backdrop_method(self, method):
+        """
+        Set the card's backdrop method
+
+        :param method: card's backdrop method
+        :type method: string
+        """
+        if method == 'none':
+            self.set_enum('card-backdrop-method', 0)
+        elif method == 'linear-gradient':
+            self.set_enum('card-backdrop-method', 1)
+        elif method == 'blurred-cover':
+            self.set_enum('card-backdrop-method', 2)
 
     @property
     def clamp_size(self):

komikku/preferences.py
@@ -27,7 +27,7 @@ class PreferencesDialog(Adw.PreferencesDialog):
     color_scheme_row = Gtk.Template.Child('color_scheme_row')
     night_light_switch = Gtk.Template.Child('night_light_switch')
     system_accent_colors_switch = Gtk.Template.Child('system_accent_colors_switch')
-    card_backdrop_switch = Gtk.Template.Child('card_backdrop_switch')
+    card_backdrop_method_row = Gtk.Template.Child('card_backdrop_method_row')
     desktop_notifications_switch = Gtk.Template.Child('desktop_notifications_switch')
     tracking_group = Gtk.Template.Child('tracking_group')
     tracking_switch = Gtk.Template.Child('tracking_switch')
@@ -89,13 +89,18 @@ class PreferencesDialog(Adw.PreferencesDialog):
     def on_borders_crop_changed(self, switch_button, _gparam):
         self.settings.borders_crop = switch_button.get_active()
 
-    def on_card_backdrop_changed(self, switch_button, _gparam):
-        if switch_button.get_active():
-            self.settings.card_backdrop = True
-            self.window.card.set_backdrop()
-        else:
-            self.settings.card_backdrop = False
+    def on_card_backdrop_method_changed(self, row, _gparam):
+        index = row.get_selected()
+
+        if index == 0:
+            self.settings.card_backdrop_method = 'none'
             self.window.card.remove_backdrop()
+        elif index == 1:
+            self.settings.card_backdrop_method = 'linear-gradient'
+            self.window.card.set_backdrop()
+        elif index == 2:
+            self.settings.card_backdrop_method = 'blurred-cover'
+            self.window.card.set_backdrop()
 
     def on_closed(self, _dialog):
         # Pop subpage if one is opened when dialog is closed
@@ -114,6 +119,7 @@ class PreferencesDialog(Adw.PreferencesDialog):
             self.settings.color_scheme = 'default'
 
         self.window.init_theme()
+        self.window.card.set_backdrop()
 
     def on_clamp_size_changed(self, adjustment):
         self.settings.clamp_size = int(adjustment.get_value())
@@ -315,9 +321,9 @@ class PreferencesDialog(Adw.PreferencesDialog):
         self.desktop_notifications_switch.set_active(self.settings.desktop_notifications)
         self.desktop_notifications_switch.connect('notify::active', self.on_desktop_notifications_changed)
 
-        # Card backdrop
-        self.card_backdrop_switch.set_active(self.settings.card_backdrop)
-        self.card_backdrop_switch.connect('notify::active', self.on_card_backdrop_changed)
+        # Card backdrop method
+        self.card_backdrop_method_row.set_selected(self.settings.card_backdrop_method_value)
+        self.card_backdrop_method_row.connect('notify::selected', self.on_card_backdrop_method_changed)
 
         # Tracking
         self.tracking_switch.set_active(self.settings.tracking)

commit def6c7e615d5e678340db30f94de12a8c73521c2

[Card] Fix regression introduced in 0cc69134

data/ui/card.blp
@@ -15,7 +15,7 @@ template $CardPage : Adw.NavigationPage {
     }
 
     [overlay]
-    Adw.ToolbarView {
+    Adw.ToolbarView toolbar_view {
       [top]
       Adw.HeaderBar {
         centering-policy: strict;

komikku/card/__init__.py
@@ -26,6 +26,7 @@ from komikku.utils import MISSING_IMG_RESOURCE_PATH
 class CardPage(Adw.NavigationPage):
     __gtype_name__ = 'CardPage'
 
+    toolbar_view = Gtk.Template.Child('toolbar_view')
     left_button = Gtk.Template.Child('left_button')
     filters_button = Gtk.Template.Child('filters_button')
     title_stack = Gtk.Template.Child('title_stack')
@@ -296,7 +297,7 @@ class CardPage(Adw.NavigationPage):
         if not self.pool_to_update:
             self.pool_to_update = True
             # Adjust revealer position
-            self.pool_to_update_revealer.set_margin_top(self.get_child().get_top_bar_height())
+            self.pool_to_update_revealer.set_margin_top(self.toolbar_view.get_top_bar_height())
             self.pool_to_update_revealer.props.transition_type = Gtk.RevealerTransitionType.NONE
             self.pool_to_update_revealer.set_reveal_child(True)
         else: