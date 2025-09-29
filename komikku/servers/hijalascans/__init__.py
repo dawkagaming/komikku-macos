0e99af53ee44ce5d8eed6295de89f81cfc85b99f
Valéry Febvre <vfebvre@easter-eggs.com>
2025-09-29 15:25:56 +0200

    [Servers] Add Hijala Scans (EN)

---
+++ komikku/servers/hijalascans/__init__.py
@@ -0,0 +1,63 @@
+# SPDX-FileCopyrightText: 2019-2025 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+import json
+import logging
+
+from bs4 import BeautifulSoup
+
+from komikku.servers.multi.heancms import extract_info_from_script
+from komikku.servers.multi.heancms import HeanCMS
+from komikku.utils import get_buffer_mime_type
+
+logger = logging.getLogger(__name__)
+
+
+class Hijalascans(HeanCMS):
+    id = 'hijalascans'
+    name = 'Hijala Scans'
+    lang = 'en'
+
+    base_url = 'https://en-hijala.com'
+    logo_url = 'https://storage.en-hijala.com/upload/2025/06/24/final-523b7e2e4fb3a659.webp'
+    api_url = 'https://api.en-hijala.com/api'
+    bypass_cf_url = base_url + '/series'
+
+    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
+        """
+        Returns manga chapter data by scraping chapter HTML page content
+
+        Pages URLs are available in a <script> element
+        """
+        r = self.session_get(
+            self.chapter_url.format(manga_slug, chapter_slug),
+            headers={
+                'Referer': self.manga_url.format(manga_slug),
+            }
+        )
+        if r.status_code != 200:
+            return None
+
+        mime_type = get_buffer_mime_type(r.content)
+        if mime_type != 'text/html':
+            return None
+
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        if info := extract_info_from_script(soup, 'images'):
+            info = json.loads(info)
+            images = info[3]['children'][1][3]['children'][3]['children'][1][3]['children'][3]['children'][3]['chapter']['images']
+
+            data = dict(
+                pages=[],
+            )
+            for image in images:
+                data['pages'].append(dict(
+                    slug=None,
+                    image=image['url'],
+                ))
+
+            return data
+
+        return None
