fbaec39852057ef061dd5640954ee3b9629300c9
Valéry Febvre <vfebvre@easter-eggs.com>
2025-10-17 19:12:53 +0200

    [Servers] Nazarick Scan (pt_BR): Disabled

--- komikku/servers/nazarickscan/__init__.py
+++ komikku/servers/nazarickscan/__init__.py
@@ -9,6 +9,7 @@ class Nazarickscan(Peachscan):
     id = 'nazarickscan'
     name = 'Nazarick Scan'
     lang = 'pt_BR'
+    status = 'disabled'
 
     base_url = 'https://nazarickscan.com.br'
     logo_url = base_url + '/static/nazarickscan.com.br/favicon-32x32.png'
