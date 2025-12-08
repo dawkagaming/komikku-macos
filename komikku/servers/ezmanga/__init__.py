82afd8d5d1259ab61fa2111ed1938fa33cf3eeae
Valéry Febvre <vfebvre@easter-eggs.com>
2025-12-08 12:19:29 +0100

    [Servers] EZmanga (EN): Update

--- komikku/servers/ezmanga/__init__.py
+++ komikku/servers/ezmanga/__init__.py
@@ -4,7 +4,12 @@
 
 import logging
 
+from bs4 import BeautifulSoup
+
 from komikku.servers.multi.heancms import HeanCMS
+from komikku.servers.utils import parse_nextjs_hydration
+from komikku.utils import get_buffer_mime_type
+from komikku.webview import CompleteChallenge
 
 logger = logging.getLogger(__name__)
 
@@ -18,3 +23,41 @@ class Ezmanga(HeanCMS):
     base_url = 'https://ezmanga.org'
     api_url = 'https://vapi.ezmanga.org/api'
     bypass_cf_url = base_url + '/series'
+
+    @CompleteChallenge()
+    def get_manga_chapter_data(self, manga_slug, manga_name, chapter_slug, chapter_url):
+        """
+        Returns manga chapter data by scraping chapter HTML page content
+
+        Pages URLs are available in a <script> element
+        """
+        r = self.session_get(
+            self.chapter_url.format(manga_slug, chapter_slug),
+            headers={
+                'Referer': self.manga_url.format(manga_slug),
+            }
+        )
+        if r.status_code != 200:
+            return None
+
+        mime_type = get_buffer_mime_type(r.content)
+        if mime_type != 'text/html':
+            return None
+
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        if info := parse_nextjs_hydration(soup, 'API_Response'):
+            images = info[2][3]['API_Response']['chapter']['images']
+
+            data = dict(
+                pages=[],
+            )
+            for image in images:
+                data['pages'].append(dict(
+                    slug=None,
+                    image=image['url'],
+                ))
+
+            return data
+
+        return None
